5bb7dd4e8aaa4e230597d9974ca28b077760e7d0
Fabio Muller <fabioml10@yahoo.com.br>
2021-06-23 11:28:31 -0300

    Add handle errors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express'
 import 'reflect-metadata'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
 import { router } from './routes'
 import './database'
 
@@ -8,5 +9,13 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return response.status(400).json({ error: error.message })
+  }
+
+  return response.status(500).json({ status: "error", message: "Inernal server error." })
+})
 
 app.listen(3001, () => { console.log('Server is running! Yeah!') })
